11c9b978d15ce3cb89c869eb5e07bb635b677fee
ay-seeing <630999015@qq.com>
2015-07-13 18:06:59 +0800

    添加gulp file include 模板项目

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp'),
+fileinclude = require('gulp-file-include');
+
+gulp.task('fileinclude', function(){
+	gulp.src('./origin/html/**.html')
+		.pipe(fileinclude({
+			prefix: '@@',
+			basepath: '@file'
+		}))
+		.pipe(gulp.dest('asset/'));
+});
