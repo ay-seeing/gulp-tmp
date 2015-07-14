61938abd90889422446a03fbd09de2ae1f183432
ay-seeing <630999015@qq.com>
2015-07-14 14:10:06 +0800

    项目添加watch和clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,30 @@
 // https://github.com/coderhaoxin/gulp-file-include
 
 var gulp = require('gulp'),
-fileinclude = require('gulp-file-include');
+fileinclude = require('gulp-file-include'),
+clean = require('gulp-clean');
+
+var paths = {
+	origin: './origin/html/**.html',
+	dest: './asset'
+}
 
 gulp.task('fileinclude', function(){
-	gulp.src('./origin/html/**.html')
+	gulp.src(paths.origin)
 		.pipe(fileinclude({
 			prefix: '@@',
 			basepath: '@file'
 		}))
-		.pipe(gulp.dest('asset/'));
+		.pipe(gulp.dest(paths.dest));
 });
+
+gulp.task('clean', function(){
+	gulp.src(paths.dest)
+		.pipe(clean({force: true}));
+});
+
+gulp.task('watch', function(){
+	gulp.watch(paths.origin, ['fileinclude']);
+});
+
+gulp.task('default', ['watch', 'fileinclude'])
